402803608cffdadef6f0216eefeeb505fb7e4aa9
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-10-14 12:45:18 +0600

    image gallery & testimonial section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -742,4 +742,22 @@ var gallery = $('.gallery .grid-item a').simpleLightbox({
 
 //My Account Collapse
 
+//Testimonial Swiper
 
+var swiper = new Swiper(".testimonial-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
